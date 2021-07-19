bbc1601ae013528aadca62a193d94667b2fa4648
Nate Sheridan <nbs@dr.com>
2021-07-18 20:45:48 -0600

    Refactor/Tidy code (comment removal)

--- src/main.js
+++ src/main.js
@@ -1,90 +1,31 @@
-// Create variables targetting the relevant DOM elements here
-
-
-// DOM VARIABLES =====================
+//Tony Hsu's and Nate Sheridan's RomCom project
 
 var title = document.querySelector('.cover-title');
 var cover = document.querySelector('.cover-image');
 var tl1 = document.querySelector('.tagline-1')
 var tl2 = document.querySelector('.tagline-2')
-
-// RELEVANT RANDOMIZED VARIABLES =====
 var randomTitle = titles[getRandomIndex(titles)];
-//declare variable randomTitle to populate data as value from the array of titles in data.js
 var randomCover = covers[getRandomIndex(covers)];
-// same as above ^
 var randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
-//same as above ^^
 var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
-//same as above but we must make sure the words are the same.
-  if (randomDescriptor1 === randomDescriptor2){
-    //if statement that says if they equal the same thing do this:
-    var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
-    //declares/refreshes the randomized descriptors
-  }
-
-//    -=-= RANDOMIZED COVER =-=-
-
-
-// UPDATE TITLE ================
-function newTitle(customTitle = randomTitle){
-  //function to invoke the new title to appear default value should be randomTitle
-  randomTitle = titles[getRandomIndex(titles)];
-  //declare randomTitle variable again to refresh the title each time the function is ran
-  title.innerText = customTitle
-  //change the text with title class in index.html to refreshed/redefine randomTitle variable.
+if (randomDescriptor1 === randomDescriptor2){
+  var randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
 }
 
-// UPDATE COVER ================  
-  function newCover(imgSrc = randomCover){
-    //function to invoke a new image to appear.
-    randomCover = covers[getRandomIndex(covers)];
-    //redeclare the randomCover variable to reload it each time the function is ran
-    cover.src = imgSrc
-    //sets html id "cover-img"'s src to "randomCover" variable.
-  }
-
-
-// UPDATE TAGLINES =============
-  function newTagline(tagline1 = randomDescriptor1, tagline2 = randomDescriptor2){
-    randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
-    randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
-  //we must make sure that each time the function is ran the descriptors are refreshed
-    tl1.innerText = tagline1
-    tl2.innerText = tagline2
-  //we must update each html value inside the span tags with classes tagline-<1/2>
-}
-
-
-// UPDATE WHOLE POSTER =========
-  function newPoster(title, tagline1, tagline2, imgSrc){ 
-    newTagline(tagline1, tagline2);
-    newCover(imgSrc);
-    newTitle(title);
-  }
-  newPoster();
-
-// CREATING CUSTOM COVER =======
-  //we need to define the input forms as variables:
-  var userTitle = document.querySelector('.user-title');
-  var userDesc1 = document.querySelector('.user-desc1')
-  var userDesc2 = document.querySelector('.user-desc2')
-  var userCover = document.querySelector('.user-cover');
-
-  //making custom poster with user input values
-  //this function should only be ran if the button is pressed.
-function makeMyBook(){
-  viewHome();
-  event.preventDefault();
-  newPoster(userTitle.value, userDesc1.value, userDesc2.value, userCover.value)
-  covers.push(userCover.value)
-  titles.push(userTitle.value)
-  // saveCoverBtn.classList.add("hidden"); // maybe prevent duplicate saves this way
-  savedCovers.push(new Poster(userTitle.value, userDesc1.value, userDesc2.value, userCover.value))
-}
-
-
-
+var userTitle = document.querySelector('.user-title');
+var userDesc1 = document.querySelector('.user-desc1')
+var userDesc2 = document.querySelector('.user-desc2')
+var userCover = document.querySelector('.user-cover');
+var homeBtn = document.querySelector('.home-button');
+var showNewCoverBtn = document.querySelector('.random-cover-button');
+var saveCoverBtn = document.querySelector('.save-cover-button');
+var viewSavedCoversBtn = document.querySelector('.view-saved-button');
+var makeYourOwnBtn = document.querySelector('.make-new-button');
+var makeMyBookBtn = document.querySelector('.create-new-book-button');
+var homeView = document.querySelector('.home-view');
+var savedView = document.querySelector('.saved-view');
+var formView = document.querySelector('.form-view');
+var savedCoverSection = document.querySelector('.saved-covers-section');
 class Poster{
   constructor(title, tl1, tl2, cover = randomCover){
     this.title = title;
@@ -93,38 +34,52 @@ class Poster{
     this.cover = cover;
     this.ID = this.title + this.tl1 + this.tl2 + this.cover;
   }
-
+  
 }
-
-
-
 
 var savedCovers = [ //saved covers should be saved in the format - Title, Desc1, Desc2, CoverImgSrc
   new Poster("Shrek", "Shrek", "more Shrek", "https://static.wikia.nocookie.net/shrek/images/7/7c/1754181-b.jpg/revision/latest/scale-to-width-down/250?cb=20210619181030"),
 ];
-
-var isDupicate
-
-//Saving covers
 var currentCover = new Poster(title.innerText, tl1.innerText, tl2.innerText, cover.src)
 
+function newTitle(customTitle = randomTitle){
+  randomTitle = titles[getRandomIndex(titles)];
+  title.innerText = customTitle
+}
+
+function newCover(imgSrc = randomCover){
+  randomCover = covers[getRandomIndex(covers)];
+  cover.src = imgSrc
+}
+
+function newTagline(tagline1 = randomDescriptor1, tagline2 = randomDescriptor2){
+  randomDescriptor1 = descriptors[getRandomIndex(descriptors)];
+  randomDescriptor2 = descriptors[getRandomIndex(descriptors)];
+  tl1.innerText = tagline1
+  tl2.innerText = tagline2
+}
+
+function newPoster(title, tagline1, tagline2, imgSrc){ 
+  newTagline(tagline1, tagline2);
+  newCover(imgSrc);
+  newTitle(title);
+}
+
+function makeMyBook(){
+  viewHome();
+  event.preventDefault();
+  newPoster(userTitle.value, userDesc1.value, userDesc2.value, userCover.value)
+  covers.push(userCover.value)
+  titles.push(userTitle.value)
+  savedCovers.push(new Poster(userTitle.value, userDesc1.value, userDesc2.value, userCover.value))
+}
 
 function saveCover(){
-var currentCover = new Poster(title.innerText, tl1.innerText, tl2.innerText, cover.src)
-    if (savedCovers.some(savedCover => savedCover.ID === currentCover.ID) === false){
+  var currentCover = new Poster(title.innerText, tl1.innerText, tl2.innerText, cover.src)
+  if (savedCovers.some(savedCover => savedCover.ID === currentCover.ID) === false){
     savedCovers.push(new Poster(title.innerText, tl1.innerText, tl2.innerText, cover.src))
   }
 }
-
-
-//populating all saved covers and adding each
-
-//saved cover section
-
-
-var inArray = (savedCovers.inArray===new Poster(title.innerText, tl1.innerText, tl2.innerText, cover.src));
-
-var savedCoverSection = document.querySelector('.saved-covers-section');
 
 function updateCovers(){
   document.querySelector('.saved-covers-section').innerHTML = ""
@@ -138,15 +93,36 @@ for (var i = 0; i < savedCovers.length; i++){
   `)   
   }
 }
-//define variables for all buttons and apply event listeners to each button
-var homeBtn = document.querySelector('.home-button');
-var showNewCoverBtn = document.querySelector('.random-cover-button');
-var saveCoverBtn = document.querySelector('.save-cover-button');
-var viewSavedCoversBtn = document.querySelector('.view-saved-button');
-var makeYourOwnBtn = document.querySelector('.make-new-button');
-var makeMyBookBtn = document.querySelector('.create-new-book-button');
 
-//event listeners for nav buttons - on click of button each function is ran
+function viewHome (){ 
+  homeView.classList.remove('hidden'); 
+  savedView.classList.add('hidden'); 
+  formView.classList.add('hidden');
+  homeBtn.classList.add('hidden'); 
+  showNewCoverBtn.classList.remove('hidden'); 
+  saveCoverBtn.classList.remove('hidden');
+}
+
+function viewSaved(){
+  savedView.classList.remove('hidden');
+  homeView.classList.add('hidden');
+  formView.classList.add('hidden');
+  saveCoverBtn.classList.add('hidden'); 
+  showNewCoverBtn.classList.add('hidden');
+  homeBtn.classList.remove('hidden');
+  currentView = savedView;
+  updateCovers();
+}
+
+function viewForm(){
+  formView.classList.remove('hidden');
+  homeView.classList.add('hidden');
+  savedView.classList.add('hidden');
+  saveCoverBtn.classList.add('hidden'); 
+  showNewCoverBtn.classList.add('hidden')
+  homeBtn.classList.remove('hidden'); 
+}
+
 homeBtn.addEventListener('click' , function(){viewHome()});
 showNewCoverBtn.addEventListener('click' , function(){newPoster()});
 saveCoverBtn.addEventListener('click' , saveCover);
@@ -154,60 +130,9 @@ viewSavedCoversBtn.addEventListener('click', function(){viewSaved()});
 makeYourOwnBtn.addEventListener('click', function(){viewForm()});
 makeMyBookBtn.addEventListener('click', function(){makeMyBook()})
 
-//define variables for access to all views provided
-var homeView = document.querySelector('.home-view');
-var savedView = document.querySelector('.saved-view');
-var formView = document.querySelector('.form-view');
+viewHome() 
+newPoster();
 
-var currentView = homeView // define a variable that tracks which view is currently being viewed by user
-
-//button functions / functionality
-  //home button
-function viewHome (){ //this function when invoked must apply the class hidden to the other views and also hide the homebutton
-  homeView.classList.remove('hidden'); //remove hidden class from homeView
-  savedView.classList.add('hidden'); // add hidden class to savedView
-  formView.classList.add('hidden') // add hidden class to formView
-  homeBtn.classList.add('hidden'); // add hidden class to home button so it doesn't show when on home view
-  showNewCoverBtn.classList.remove('hidden'); // shows NewCoverBtn
-  saveCoverBtn.classList.remove('hidden'); // shows saveCoverBtn
-}
-viewHome() // we want to run the function on page load to make sure the home button is hidden by default
-
-  //view saved covers button
-function viewSaved(){
-  savedView.classList.remove('hidden');
-  homeView.classList.add('hidden');
-  formView.classList.add('hidden');
-  saveCoverBtn.classList.add('hidden'); // we only want to see the "save cover" button while on home view
-  showNewCoverBtn.classList.add('hidden') // we only want to show the "Show new Random Cover" on home view
-  homeBtn.classList.remove('hidden'); // make sure the home buttom is showing up when on this view
-  currentView = savedView;
-  updateCovers();
-}
-
-  //make your own cover button
-function viewForm(){
-  formView.classList.remove('hidden');
-  homeView.classList.add('hidden');
-  savedView.classList.add('hidden');
-  saveCoverBtn.classList.add('hidden'); // we only want to see the "save cover" button while on home view
-  showNewCoverBtn.classList.add('hidden') // we only want to show the "Show new Random Cover" on home view
-  homeBtn.classList.remove('hidden'); // make sure the home buttom is showing up when on this view
-}
-
-
-
-// We've provided a few variables below
-
-
-
-// Add your event listeners here 👇
-
-
-// Create your event handlers and other functions here 👇
-
-
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
